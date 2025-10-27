c62fed95ebb8fe106a55379cc14dde6e696280d0
bmdoctor7 <2081476659@qq.com>
2025-10-28 01:29:38 +0800

    摆钟逻辑

--- Assets/Scipts/ItemComponent/Items/Bell.cs
+++ Assets/Scipts/ItemComponent/Items/Bell.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Spine;
+using Spine.Unity;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -9,6 +11,7 @@ public class Bell : Item
     private InteractableComponent interactableComponent;
     public UnityEvent OnInteract;
     
+    public SkeletonAnimation skeletonAnimation; // 骨骼动画组件
     public GameObject skeletonPrefab;
 
     private void Start()
@@ -34,15 +37,49 @@ public class Bell : Item
     {
         base.Used();
         OnInteract?.Invoke();
+        Click();
     }
 
     private void Click()
     {
-        SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
-        spriteRenderer.sprite = Resources.Load<Sprite>("Art/Bell");
-        StartCoroutine(CountTime());
+        Debug.Log("BellClick");
+        skeletonAnimation.state.SetAnimation(0, "posun1", false);
+        PlayAnimationWaitLastAnimation("posun2");
+        //StartCoroutine(CountTime());
     }
 
+    
+    public void PlayAnimationWaitLastAnimation(string animationName)
+    {
+        if (skeletonAnimation == null)
+            return;
+
+        // 获取当前正在播放的动画
+        var currentTrackEntry = skeletonAnimation.state.GetCurrent(0);
+
+        if (currentTrackEntry != null)
+        {
+            // 如果有正在播放的动画，则等待其完成后再播放新动画
+            StartCoroutine(WaitForAnimationComplete(currentTrackEntry, animationName));
+        }
+        else
+        {
+            // 如果没有正在播放的动画，直接播放新动画
+            skeletonAnimation.state.SetAnimation(0, animationName, true);
+        }
+    }
+    private IEnumerator WaitForAnimationComplete(TrackEntry trackEntry, string animationName)
+    {
+        // 等待当前动画播放完毕
+        while (trackEntry.IsComplete == false)
+        {
+            yield return null;
+        }
+
+        // 播放新动画，不循环播放
+        skeletonAnimation.state.SetAnimation(0, animationName, true);
+    }
+    
     protected override IEnumerator CountTime()
     {
         yield return new WaitForSeconds(0.1f);
