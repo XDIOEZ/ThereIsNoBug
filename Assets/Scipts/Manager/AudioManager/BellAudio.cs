1fc9ab2ae8a9cff18b5ad977315d583f0ca290ea
Ice_Tilapia <202332793@qq.com>
2025-10-29 01:02:11 +0800

    修复钟表设置

---
+++ Assets/Scipts/Manager/AudioManager/BellAudio.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class BellAudio : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public canInteract canInteract;
+    
+    public void Update()
+    {
+        if(currentPosition.Instance.Y_currentindex == 3 && currentPosition.Instance.X_currentindex == 1)
+        {
+            // TODO 在播放的时候不覆盖
+            // 修改为：只有在音频未播放时才开始播放
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+        }
+        else
+        {
+            audioSource.Stop();
+        }
+
+        if(canInteract.enabled == false)
+        {
+            audioSource.Stop();
+        }
+    }
+}
