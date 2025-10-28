68e28f2af14c42568dc5af27e22ff37ac9bb6519
bmdoctor7 <2081476659@qq.com>
2025-10-28 21:26:14 +0800

    神秘小羊

--- Assets/Scipts/ItemComponent/Items/Sheep.cs
+++ Assets/Scipts/ItemComponent/Items/Sheep.cs
@@ -1,20 +1,65 @@
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 
 public class Sheep : Item
 {
     private InteractableComponent InteractableComponent;
 
+    [Header("晃动设置")]
+    public float shakeDuration = 0.5f;
+    public float shakeStrength = 0.5f;
+    public int shakeVibrato = 10;
+    public float shakeRandomness = 90f;
+    private Vector3 originalPosition;
+    private bool isShaking = false;
+    private bool isOut;
+
     private void Start()
     {
         InteractableComponent = GetComponent<InteractableComponent>();
         InteractableComponent.OnInteract += Used;
+        originalPosition = transform.position;
     }
 
     protected override void Used()
     {
         base.Used();
-        gameObject.SetActive(false);
+        
+        if(!isShaking)
+            ShakeObject();
+        if (!isOut)
+        {
+            this.gameObject.GetComponentInChildren<SpriteRenderer>().sortingOrder++;
+            isOut= true;
+        }
+        else
+        {
+            isOut = false;
+            this.gameObject.GetComponentInChildren<SpriteRenderer>().sortingOrder--;
+        }
+            
+            
+        
     }
+    
+    
+    
+    void ShakeObject()
+    {
+        isShaking = true;
+        
+        // 使用Dotween实现晃动效果
+        transform.DOShakePosition(shakeDuration, shakeStrength, shakeVibrato, shakeRandomness)
+            .OnComplete(() => 
+            {
+                // 晃动结束后回到原位置
+                transform.position = originalPosition;
+                isShaking = false;
+            });
+    }
+    
+    
+    
 }
